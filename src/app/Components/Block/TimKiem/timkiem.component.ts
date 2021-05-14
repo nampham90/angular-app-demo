7785d0b515647a9a3e3d2b6ef3eb768402910f84
nampham90 <namandroid.it@gmail.com>
2021-05-14 12:28:33 +0700

    make it better

--- src/app/Components/Block/TimKiem/timkiem.component.ts
+++ src/app/Components/Block/TimKiem/timkiem.component.ts
@@ -1,15 +1,48 @@
 import {Component,OnInit } from "@angular/core";
 
+import {LoaibdsService} from '../../../Services/loaibds.module';
+
+import {FormGroup,FormControl} from '@angular/forms';
 
 
 
 @Component({
     templateUrl: 'timkiem.component.html',
     selector: 'timkiem',
+    providers: [LoaibdsService]
     
 })
 
-export class TimkiemComponent { 
+export class TimkiemComponent implements OnInit{
+  loaibsd: any;
+
+  public formSeach: FormGroup;
+
+  constructor(
+    private loadbdsService: LoaibdsService
+  ){
+     
+      this.formSeach=new FormGroup({
+        phanloai: new FormControl('BDS Thuê'),
+        loaibds : new FormControl('Căn Hộ Cao Cấp'),
+        khuvuc : new FormControl('Thành Phố Huế')
+      });
+  }
+  ngOnInit(): void {
+    this.retrieveLoadbds();
+  } 
+
+  retrieveLoadbds(): void {
+    this.loadbdsService.getAll()
+      .subscribe(
+        data => {
+          this.loaibsd = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+}
 
   
 }
