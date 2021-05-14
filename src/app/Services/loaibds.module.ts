e0fc1281e4c60cf27bcd189b729a7d597fbee612
nampham90 <namandroid.it@gmail.com>
2021-05-15 00:22:29 +0700

    make it better

--- src/app/Services/loaibds.module.ts
+++ src/app/Services/loaibds.module.ts
@@ -1,14 +1,24 @@
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs';
+import { Observable,throwError  } from 'rxjs';
 import {HttpClient,HttpHeaders} from "@angular/common/http";
+import { catchError, map } from 'rxjs/operators';
 
-const baseUrl = 'https://192.168.1.26:3443/api/loaibds/';
+const baseUrl = 'https://192.168.1.26:3443/api/loaibds';
 
 @Injectable()
 
 export class LoaibdsService{
-    constructor( private http: HttpClient){}
+    constructor(
+         private http: HttpClient
+         
+         ){}
     getAll(): Observable<any> {
-        return this.http.get(baseUrl);
+        const httpHeaders=new HttpHeaders({
+                  'Content-Type': 'application/json',
+                  'X-Requested-With': 'XMLHttpRequest',
+                  'MyClientCert': '',    
+                  'MyToken': ''            
+        })
+        return this.http.get(baseUrl,{headers:httpHeaders}).pipe( map(res => res), catchError(err => throwError(err)));
     }
 }
